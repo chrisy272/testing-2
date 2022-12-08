d00df65d670ad281e1e60d6489165112883bd8a4
chrisy272 <96126247+chrisy272@users.noreply.github.com>
2022-12-08 06:17:34 -0500

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        title: `Casey Gatsby Site First one :)`,
+        author: `Casey Gerena (CJ)`,
+        description: `Hey this is my first site, sort of.`
+    },
+    plugins: []
+}
