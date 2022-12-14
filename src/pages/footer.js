ffaf59e0925b92777014d53d0961014517c35f52
chrisy272 <96126247+chrisy272@users.noreply.github.com>
2022-12-14 12:43:20 +0000

    changed stuff in footer

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -2,5 +2,20 @@ import React from 'react';
 
 
 const FooterPage = () => {
-    return ()
+
+    return (
+        <>
+
+            <h1>anything you want , but we'll call this the footer</h1>
+            <footer>
+                <p>hey this is my footer</p>
+                <div>
+                    <p>hey this is inside div tag!</p>
+                </div>
+            </footer>
+        </>
+
+    );
 }
+
+export default FooterPage;
